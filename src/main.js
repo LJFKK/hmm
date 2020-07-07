ad55c0a54c0a82f888ee2cfc98648f5b19ce9d5c
LJFKK <66809690+LJFKK@users.noreply.github.com>
2020-07-07 11:31:13 +0800

    项目起步-elementui整合

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入js ele
+import ElementUI from 'element-ui'
+//导入css ele
+import 'element-ui/lib/theme-chalk/index.css'
 
+//注册 ele
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
